7d316e44210a083d1487e0c9e4c8d0eb9d3622ef
DUDI DINESH <208412574+Dinesh2006-Dudi@users.noreply.github.com>
2026-09-13 11:08:51 +0530

    Add: counting-bits - submission-0

---
+++ Data Structures & Algorithms/counting-bits/submission-0.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] countBits(int n) {
+        
+        int ans[]=new int[n+1];
+
+        for(int i=0;i<=n;i++)
+        {
+            int x=i;
+
+            while(x>0)
+            {
+                if(x%2==1)
+                {
+                    ans[i]++;
+                }
+                x/=2;
+            }
+        }
+        return ans;
+    }
+}
